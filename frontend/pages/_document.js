6d80e9c733f22f4a44db02e0befa774bd6987fd3
Ryaan <147352765+rfarrukh0@users.noreply.github.com>
2025-05-18 17:27:32 -0400

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>MarketVision | AI Product Generator</title>
+        <meta name="description" content="Generate full marketing content from a single image." />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
